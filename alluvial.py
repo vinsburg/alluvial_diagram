546648ce5e92842a15bd30731ea1f4c42af42198
vinsburg <vinsburg@gmail.com>
2018-01-21 12:45:17 +0200

    Update alluvial.py

--- alluvial.py
+++ alluvial.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 import matplotlib.cm
 import itertools
+# import bidi.algorithm  # for RTL languages
 
 
 def plot(input_data, *args, **kwargs):
@@ -24,13 +25,16 @@ class AlluvialTool:
         self.data_dic = self.read_input()
         self.item_widths_dic = self.get_item_widths_dic()
         self.a_members, self.b_members = self.get_item_groups(**kwargs)
+        self.h_gap_frac = h_gap_frac
         self.h_gap = x_range[1] * h_gap_frac
         self.v_gap_frac = v_gap_frac
         self.v_gap = sum(
             [width for b_item_counter in self.data_dic.values() for width in b_item_counter.values()]
         ) * v_gap_frac
+        self.group_widths = self.get_group_widths()
         self.item_coord_dic = self.make_item_coordinate_dic()
         self.alluvial_fan = self.generate_alluvial_fan()
+        self.item_text_len, self.width_text_len = self.get_vein_label_lengths()
 
     def make_vein_blueprint_xy_arrays(self):
         y = np.array([0, 0.15, 0.5, 0.85, 1])
@@ -51,14 +55,14 @@ class AlluvialTool:
         y_new = np.concatenate([ty, ty[::-1] + width, ])
         return np.array([x_new, y_new]).transpose()
 
-    def read_input_from_table(self):
+    def read_input_from_list(self):
         data_table = np.array(self.input)
         data_dic = defaultdict(Counter)
         for line in data_table:
             data_dic[line[0]][line[1]] += 1
         return data_dic
 
-    def read_input_from_dic(self):
+    def read_input_from_dict(self):
         # data_dic = self.input
         # data_table = []
         # for x_item, y_item_counter in data_dic.items():
@@ -69,10 +73,10 @@ class AlluvialTool:
         return self.input
 
     def read_input(self):
-        if type(self.input) == dict:
-            return self.read_input_from_dic()
-        else:
-            return self.read_input_from_table()
+        return {
+            dict: self.read_input_from_dict(),
+            list: self.read_input_from_list()
+         }[type(self.input)]
 
     def get_item_widths_dic(self):
         iwd = Counter()
@@ -93,10 +97,13 @@ class AlluvialTool:
         ) if not b_sort else b_sort
         return a_members, b_members
 
+    def get_group_widths(self):
+        return [self.get_group_width(group) for group in (self.a_members, self.b_members)]
+
     def make_item_coordinate_dic(self, ):
         item_coord_dic = defaultdict(ItemCoordRecord)
         groups = self.a_members, self.b_members
-        group_widths = [self.get_group_width(group) for group in groups]
+        group_widths = self.group_widths
         for ind, group in enumerate(groups):
             last_pos = (max(group_widths) - group_widths[ind]) / 2
             for item in group:
@@ -159,17 +166,17 @@ class AlluvialTool:
             for patch in patches:
                 ax.add_patch(patch)
         self.auto_label_veins(**kwargs)
+        self.label_sides(**kwargs)
         ax.autoscale()
         return ax
 
-    def get_color_array(self, colors=None, color_side=0, rand_seed=1,
-                        cmap=None, **kwargs):
+    def get_color_array(self, colors=None, color_side=0, rand_seed=1, cmap=None, **kwargs):
         _ = kwargs
         color_items = self.b_members if color_side else self.a_members
         lci = len(color_items)
         if rand_seed is not None:
             np.random.seed(rand_seed)
-        cmap = cmap if cmap is not None else matplotlib.cm.get_cmap('hsv', lci*10**2)
+        cmap = cmap if cmap is not None else matplotlib.cm.get_cmap('hsv', lci * 10 ** 3)
         color_array = colors if colors is not None else [
             cmap(item) for ind, item in enumerate(np.random.rand(lci))]
         ind_dic = {item: ind for ind, item in enumerate(color_items)}
@@ -178,24 +185,42 @@ class AlluvialTool:
             item = b_item if color_side else a_item
             polygon_colors += [color_array[ind_dic[item]]]
         return np.array(polygon_colors)
-    
+
+    def get_vein_label_lengths(self):
+        item_text_len = max([len(it) for it in self.item_widths_dic])
+        width_text_len = max([len(str(w)) for w in self.item_widths_dic.values()])
+        return item_text_len, width_text_len
+
     def auto_label_veins(self, fontname='Arial', **kwargs):
         # shift = max([len(item) for item in self.item_coord_dic.keys()]) / 50
-        item_text_len = max([len(it) for it in self.item_coord_dic])
-        width_text_len = max([len(str(w)) for w in self.item_widths_dic.values()])
         for item, vein in self.item_coord_dic.items():
             y_width = vein.get_width()
             sign = vein.get_side_sign()
             side = int(sign + 1) // 2
-            ha = 'right' if sign == -1 else 'left'
+            ha = 'left' if side else 'right'
             plt.text(
                 vein.get_x() + 1.5 * sign * self.h_gap,
                 vein.get_y() + y_width / 2,
-                self.item_text(item, item_text_len, width_text_len, side, **kwargs),
+                self.item_text(item, side, **kwargs),
                 ha=ha, va='center', fontname=fontname)
 
+    def label_sides(
+            self, labels=None, disp_width=False, wdisp_sep=7*' ', fontname='Arial', **kwargs):
+        if labels is not None:
+            _ = kwargs
+            y = max(self.group_widths)/2
+            itl, wtl = self.item_text_len, self.width_text_len
+            for side, sign in enumerate((-1, 1)):
+                plt.text(
+                    self.x_range[side]+sign*(itl+int(disp_width)*(len(wdisp_sep)+wtl))*self.h_gap_frac/1.7,
+                    y,
+                    labels[side]
+                    # bidi.algorithm.get_display(labels[side]),  # RTL languages
+                    ha='center', va='center', fontname=fontname, fontsize=13, rotation=90-180*side
+                )
+
     def item_text(
-            self, item, item_text_len, width_text_len, side,
+            self, item, side,
             disp_width=False, wdisp_sep=7*' ', width_in=True, **kwargs):
         _ = kwargs
         f_item = item
@@ -206,9 +231,9 @@ class AlluvialTool:
         else:
             width = self.item_coord_dic[item].get_width()
             if side and width_in or (not side and not width_in):
-                lc, rc, wl, wr, tl, tr = '>', tal, width_text_len, item_text_len, width, f_item,
+                lc, rc, wl, wr, tl, tr = '>', tal, self.width_text_len, self.item_text_len, width, f_item,
             else:
-                lc, rc, wl, wr, tl, tr = tal, '>', item_text_len, width_text_len, f_item, width,
+                lc, rc, wl, wr, tl, tr = tal, '>', self.item_text_len, self.width_text_len, f_item, width,
             pat = '{:%s%d}%s{:%s%d}' % (lc, wl, wdisp_sep, rc, wr,)
             ans = pat.format(tl, tr, )
         return ans
